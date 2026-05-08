8932ed5d1ce93fb051d27b757fcb7e2afae93296
ytta0301 <juanpelamoniaerson@gmail.com>
2026-05-08 19:53:24 +0700

    feat: update testimoni page to use dynamic data and refactor routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GeminiController;
 use App\Http\Controllers\HistoryController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TestimoniController;
 
 // Guest only
 Route::middleware('guest')->group(function () {
@@ -23,7 +24,6 @@ Route::get('/dashboard', fn() => view('dashboard.dashboard'));
 Route::get('/order', fn() => view('order.order'));
 Route::get('/payment', fn() => view('payment.payment'));
 Route::get('/portofolio', fn() => view('portofolio.portofolio'));
-Route::get('/testimoni', fn() => view('testimoni.testimoni'));
 Route::get('/history', [HistoryController::class, 'index'])->name('history');
 Route::get('/admin', fn() => view('admin.admin'));
 
@@ -31,6 +31,8 @@ Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
 Route::get('/profile/{id}/edit', [ProfileController::class, 'edit'])->name('profile.edit');
 Route::put('/profile/{id}', [ProfileController::class, 'update'])->name('profile.update');
 
+Route::get('/testimoni', fn() => view('testimoni.testimoni'));
+Route::get('/testimoni', [TestimoniController::class, 'index']);
 
 // Chatbot
 Route::get('/chatbot', [GeminiController::class, 'index'])->name('chatbot.index');
