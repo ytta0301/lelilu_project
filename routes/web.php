098b819b991a563261c8a70e12444d44ac56d6bf
KukuhFauzy <Muhammad_Prasetyadi_ts7_24@student.smktelkom-sda.sch.id>
2026-05-08 08:17:54 +0700

    Testimoni part-1

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GeminiController;
 use App\Http\Controllers\HistoryController;
+use App\Http\Controllers\TestimoniController;
 
 // Guest only
 Route::middleware('guest')->group(function () {
@@ -22,7 +23,8 @@ Route::get('/dashboard', fn() => view('dashboard.dashboard'));
 Route::get('/order', fn() => view('order.order'));
 Route::get('/payment', fn() => view('payment.payment'));
 Route::get('/portofolio', fn() => view('portofolio.portofolio'));
-Route::get('/testimoni', fn() => view('testimoni.testimoni'));
+Route::get('/profile', fn() => view('profile.profile'));
+Route::get('/testimoni', [TestimoniController::class, 'index']);
 Route::get('/history', [HistoryController::class, 'index'])->name('history');
 Route::get('/admin', fn() => view('admin.admin'));
 
